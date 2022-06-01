ec06e0441980a24b6225899d9bf611adfcd20591
Lenovo <Lenovo@DESKTOP-JR31K0S>
2022-06-01 09:20:10 +0530

    update primenumber.

--- KodyTask/src/com/kodytechnolab/PrimeNumber.java
+++ KodyTask/src/com/kodytechnolab/PrimeNumber.java
@@ -1,23 +1,21 @@
+/*This Progrm prints prime numbers between 1 to 100.*/
 package com.kodytechnolab;
 
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		int j =0;
-		
-		for(int i = 1; i <= 100; i++)
-		{
-			for(j = 2; j <= i; j++)
-			{
-				if(i % j == 0)
-				{
+		/* j variable which check that the number is divided from 2 to itself. */
+		int j = 0;
+
+		for (int i = 1; i <= 100; i++) {
+			for (j = 2; j <= i; j++) {
+				if (i % j == 0) {
 					break;
 				}
 			}
-			if(i == j)
-			{
+			/* compare i and j variable if it is same number is prime and print it. */
+			if (i == j) {
 				System.out.print(i + " ");
 			}
 		}
